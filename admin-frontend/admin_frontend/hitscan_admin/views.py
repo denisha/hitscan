e2d9aad203de7d537e834049ffbf28fb0263620e
YongamaDilima <yongama.dilima@takealot.com>
2018-03-02 00:04:21 +0200

    Revert "Merge branch 'master' of https://github.com/denisha/hitscan into yongamad"
    This reverts commit 003783ccef38b0318ed20dfb30d3c1d5681a2a3e, reversing
    changes made to ed17087e1bd4f0f62125e9006eee235b44cee7a8.

--- admin-frontend/admin_frontend/hitscan_admin/views.py
+++ admin-frontend/admin_frontend/hitscan_admin/views.py
@@ -1,34 +1,5 @@
 from django.http import HttpResponse
-import pyrebase
-
- # Initialize Firebase
-config = {
-    'apiKey': 'AIzaSyCWi_KKa2NyJ2mDH7xjiU0RZK4fFOcbq-w',
-    'authDomain': 'hitscan-25b2a.firebaseapp.com',
-    'databaseURL': 'https://hitscan-25b2a.firebaseio.com',
-    'projectId': 'hitscan-25b2a',
-    'storageBucket': 'hitscan-25b2a.appspot.com',
-    'messagingSenderId': '988111583129'
-  }
-
-firebase = pyrebase.initialize_app(config)
-
 
 
 def index(request):
-    auth = firebase.auth()
-    user = auth.sign_in_with_email_and_password('khayelihle.tshuma@takealot.com', 'password')
-    db = firebase.database()
-
-    data = {
-        'IMEI': 123456,
-        'deviceModel': 'ks360',
-        'discription': 'its a phone',
-        'serialNumber': 12345}
-
-    # Pass the user's idToken to the push method
-    results = db.child("DEVICE").push(data, user['idToken'])
-
-    # Pass the user's idToken to the push method
-    results = db.child("DEVICE").get(user['idToken'])
-    return HttpResponse('This is HitScan.')
+    return HttpResponse("This is HitScan.")
