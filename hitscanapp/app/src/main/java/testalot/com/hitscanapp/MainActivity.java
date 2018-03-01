48ba78d2287e1d84365126346c02f2d96dc3dee6
Lisa Bailey <lisa.bailey@takealot.com>
2018-03-01 21:29:25 +0200

    added sound file code, new pages + images

--- hitscanapp/app/src/main/java/testalot/com/hitscanapp/MainActivity.java
+++ hitscanapp/app/src/main/java/testalot/com/hitscanapp/MainActivity.java
@@ -3,27 +3,16 @@ package testalot.com.hitscanapp;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
 import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mysound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
-
-public class MainActivity extends Activity {
-   MediaPlayer mysound;
-   @Override
-    protected void onCreate(Bundle savedInstanceState){
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
-   }
-
-    
-
-
 
 }
