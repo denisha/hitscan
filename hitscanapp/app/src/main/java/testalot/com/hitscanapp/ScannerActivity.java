5f2ecce228000fc97329f70f7e15f44e4a0d3236
Denisha <denisha@takealot.com>
2018-03-02 13:03:21 +0200

    added intent to move to next page

---
+++ hitscanapp/app/src/main/java/testalot/com/hitscanapp/ScannerActivity.java
@@ -0,0 +1,19 @@
+package testalot.com.hitscanapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.androidjson.firebasegooglelogin_androidjsoncom.R;
+
+public class ScannerActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+}
